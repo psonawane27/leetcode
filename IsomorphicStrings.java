cc8ce04541c69af88f96789d284fdb409d4d43f2
psonawane <psonawane@cs.stonybrook.edu>
2017-01-03 00:28:17 +0530

    adding solution for isomorphic strings

---
+++ IsomorphicStrings.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> m = new HashMap<Character, Character>();
+        char sChar, tChar;
+        for(int i=0; i<s.length();i++){
+            sChar = s.charAt(i);
+            tChar = t.charAt(i);
+            if(!m.containsKey(sChar)){
+                if(m.containsValue(tChar))
+                    return false;
+                m.put(sChar, tChar);
+            }
+            else{
+                System.out.println(m.get(sChar));
+                if(m.get(sChar) != tChar)
+                    return false;
+            }
+        }
+        return true;
+    }
+}

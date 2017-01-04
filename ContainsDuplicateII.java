eab779ca91b8b2fac4cd21f571a6ff4fb0c5f0fc
psonawane <psonawane@cs.stonybrook.edu>
2017-01-04 10:12:37 +0530

    adding solution for ContainsDuplicateII

---
+++ ContainsDuplicateII.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ContainsDuplicateII {
+	public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();
+        int j;
+        for(int i=0; i<nums.length; i++){
+            if(m.containsKey(nums[i])){
+                j = m.get(nums[i]);
+                if(i-j <= k)
+                    return true;
+            }
+            m.put(nums[i],i);
+        }
+        return false;
+    }
+}

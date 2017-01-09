9f0f6bd6ef6f4cc80abdde28cbce9b88b808b5e7
psonawane <psonawane@cs.stonybrook.edu>
2017-01-09 15:51:42 +0530

    updating IsomorphicStrings

--- IsomorphicStrings.java
+++ IsomorphicStrings.java
@@ -1,11 +1,13 @@
 /*
-Space complexity: O(N)
+Space complexity: O(1)
 Time Complexity: O(N)
 */
 import java.util.*;
 
 public class IsomorphicStrings {
-    public boolean isIsomorphic(String s, String t) {
+    /*
+	Space complexity: O(N)
+	public boolean isIsomorphic(String s, String t) {
         HashMap<Character, Character> m = new HashMap<Character, Character>();
         char sChar, tChar;
         for(int i=0; i<s.length();i++){
@@ -20,6 +22,14 @@ public class IsomorphicStrings {
                 if(m.get(sChar) != tChar)
                     return false;
             }
+        }
+        return true;
+    }*/
+	public boolean isIsomorphic(String s1, String s2) {
+        int[] m = new int[512];
+        for (int i = 0; i < s1.length(); i++) {
+            if (m[s1.charAt(i)] != m[s2.charAt(i)+256]) return false;
+            m[s1.charAt(i)] = m[s2.charAt(i)+256] = i+1;
         }
         return true;
     }

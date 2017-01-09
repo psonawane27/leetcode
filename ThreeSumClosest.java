6a876be6d722f0b568715a17a56cab437c80351b
psonawane <psonawane@cs.stonybrook.edu>
2017-01-09 07:01:05 +0530

    updating ThreeSumClosest

--- ThreeSumClosest.java
+++ ThreeSumClosest.java
@@ -1,25 +1,31 @@
 /*
 Space complexity: O(1)
-Time Complexity: O(N^3)
+Time Complexity: O(N^2)
 */
 import java.util.*;
 
 public class ThreeSumClosest {
 	public int threeSumClosest(int[] nums, int target) {
-        int minDiff=Integer.MAX_VALUE, closeSum=0;
-        int diff, sum;
-        for(int i=0; i<nums.length-2; i++){
-            for(int j=i+1; j<nums.length-1; j++){
-                for(int k=j+1; k<nums.length; k++){
-                    sum = nums[i] + nums[j] + nums[k];
-                    diff = Math.abs(sum - target);
-                    if(diff == 0){
-                        return sum;
-                    }
-                    if(diff < minDiff){
-                        minDiff = diff;
+        int diff=Integer.MAX_VALUE, closeSum=0;
+        int sum;
+        Arrays.sort(nums);
+        for(int k=0; k<nums.length-2; k++) {
+            for(int i=k+1, j=nums.length-1; i<j; ) {
+                sum = nums[k] + nums[i] + nums[j];
+                if (sum == target)
+                    return sum;
+                else if (sum > target) {
+                    if (sum-target < diff) {
+                        diff = sum-target;
                         closeSum = sum;
                     }
+                    j--;
+                } else {
+                    if(target-sum < diff) {
+                        diff = target-sum;
+                        closeSum = sum;
+                    }
+                    i++;
                 }
             }
         }
